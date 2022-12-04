c74ad67ae5025bdb3fed591215c236b01c706651
David Liu <david-khala@hotmail.com>
2022-12-04 11:19:00 +0800

    Update compose.sh

--- superset/compose.sh
+++ superset/compose.sh
@@ -2,6 +2,7 @@
 curl https://raw.githubusercontent.com/davidkhala/davidkhala/main/git.sh | bash -s update-clone https://github.com/apache/superset.git
 
 cd superset
+git checkout 1.5.2
 rebuild(){
   docker compose build --force-rm
  
